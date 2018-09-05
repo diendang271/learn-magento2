4e70fd33a8b0f3464c289198991585e84a2bb3ae
Dien Dang <lulzsecd@gmail.com>
2018-09-05 15:56:49 +0700

    Create custom Admin Menu

---
+++ AdminMenu/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Craig
+ * Date: 9/5/2018
+ * Time: 3:39 PM
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    "Craig_AdminMenu",
+    __DIR__
+);
